30faac57857c85480120cfd792ad020f4a37246d
ThordisLia <67345320+ThordisLia@users.noreply.github.com>
2020-11-25 20:12:21 -0600

    starter code

---
+++ src/main/java/com/hiddengems/hiddengems/HiddenGemsApplication.java
@@ -0,0 +1,13 @@
+package com.hiddengems.hiddengems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiddenGemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiddenGemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiddengems/hiddengems/HiddenGemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiddengems.hiddengems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiddenGemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
