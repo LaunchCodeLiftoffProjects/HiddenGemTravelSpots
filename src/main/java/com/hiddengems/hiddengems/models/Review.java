6bbf72eea12bad7a292e3c1f9c5dbdd147b86cf7
tvalli3 <41927664+tvalli3@users.noreply.github.com>
2020-12-14 22:36:22 -0600

    Added Review template, controller, data model.  Updated link to Review on index template.

--- src/main/java/com/hiddengems/hiddengems/models/Review.java
+++ src/main/java/com/hiddengems/hiddengems/models/Review.java
@@ -1,13 +1,54 @@
 package com.hiddengems.hiddengems.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
 @Entity
 public class Review extends AbstractEntity{
 
     @NotNull
-    private boolean thumbsup;
+    private String thumbsup;
 
+//    @ManyToOne
+//    private Gem gem;
 
+    @ManyToOne
+    private User user;
+
+    private String reviewText;
+
+    public Review() {}
+
+    public String getThumbsup() {
+        return thumbsup;
+    }
+
+    public void setThumbsup(String thumbsup) {
+        this.thumbsup = thumbsup;
+    }
+
+    //    public Gem getGem() {
+//        return gem;
+//    }
+//
+//    public void setGem(Gem gem) {
+//        this.gem = gem;
+//    }
+//
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
 }
