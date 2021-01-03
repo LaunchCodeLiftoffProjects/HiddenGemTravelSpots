0d2e3c3543de44bcc6e8987cfbd6cc613009929a
tvalli3 <41927664+tvalli3@users.noreply.github.com>
2021-01-03 01:30:32 -0600

    Settings loads and submits but doesn't redirect properly.

--- src/main/java/com/hiddengems/hiddengems/controllers/UserProfileController.java
+++ src/main/java/com/hiddengems/hiddengems/controllers/UserProfileController.java
@@ -8,6 +8,7 @@ import com.hiddengems.hiddengems.models.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -57,10 +58,14 @@ public class UserProfileController {
     }
 
     @PostMapping("/profile/settings")
-    public String processUserProfileSettings() {
+    public String processUserProfileSettings(UserProfile userProfile, Errors errors, Model model) {
 
+        if(errors.hasErrors()) {
+            return "settings";
+        }
 
+        userProfileRepository.save(userProfile);
 
-        return "redirect:../index.html";
+        return "redirect:../index";
     }
 }
